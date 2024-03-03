f5cbf35470af7d9745114d2247b7eb079b1f70c3
Mihail <Misha200417@gmail.com>
2024-03-03 21:15:23 +0300

    added config.py and class ProjectConfig with mongo and fastapi hosts

---
+++ config.py
@@ -0,0 +1,16 @@
+from dotenv import find_dotenv, load_dotenv
+from starlette.config import Config
+from os import environ
+
+
+config = Config('.env')
+load_dotenv(find_dotenv())
+
+
+class ProjectConfig:
+
+    MONGO_HOST = environ.get('MONGO_HOST')
+    MONGO_PORT = environ.get('MONGO_PORT')
+    MONGO_URL = f'mongodb://{MONGO_HOST}:{MONGO_PORT}/'
+
+    FASTAPI_HOST = environ.get('FASTAPI_HOST')
