2b88c94b6ddf053b4a7e0b15e5042ba3287f64bc
Mihail <Misha200417@gmail.com>
2024-03-03 21:16:31 +0300

    added auto launch uvicorn

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import uvicorn
+
 from fastapi import FastAPI
+from config import ProjectConfig
 from router import router as files
 
 
 app = FastAPI()
 app.include_router(files)
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', reload=True, ws_ping_interval=300, ws_ping_timeout=300, host=ProjectConfig.FASTAPI_HOST)
