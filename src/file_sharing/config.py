d193c990b74a214e651a1589862dfd97843d45c5
Mihail <Misha200417@gmail.com>
2024-03-05 05:56:08 +0300

    added environment variables to the config

--- src/file_sharing/config.py
+++ src/file_sharing/config.py
@@ -11,7 +11,13 @@ class ProjectConfig:
 
     MONGO_HOST = environ.get('MONGO_HOST')
     MONGO_PORT = environ.get('MONGO_PORT')
-    MONGO_URL = f'mongodb://{MONGO_HOST}:{MONGO_PORT}/'
+    MONGO_URL = f'mongodb://{MONGO_HOST}:{MONGO_PORT}/files_info'
 
     FASTAPI_HOST = environ.get('FASTAPI_HOST')
+
+    MINIO_HOST = environ.get('MINIO_HOST')
+    MINIO_PORT = environ.get('MINIO_PORT')
+    MINIO_ROOT_USER = environ.get('MINIO_ROOT_USER')
+    MINIO_ROOT_PASSWORD = environ.get('MINIO_ROOT_PASSWORD')
+    MINIO_URL = f'{MINIO_HOST}:{MINIO_PORT}'
     
