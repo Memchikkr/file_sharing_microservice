2f89c11c841d63e5e5f3313e94bc6fd5670b8a98
Mihail <Misha200417@gmail.com>
2024-03-14 04:06:19 +0300

    redesigned the mock minio

--- src/file_sharing/tests/conftest.py
+++ src/file_sharing/tests/conftest.py
@@ -5,21 +5,16 @@ from httpx import AsyncClient
 from beanie import init_beanie
 from src.main import app
 from mongomock_motor import AsyncMongoMockClient
+from minio import Minio
 from pytest_minio_mock.plugin import MockMinioClient
 
 from src.config import ProjectConfig
+from src.file_sharing.tests.fixtures.fixture_to_upload import fixture_give_upload_file
 
 # MONGO
 mongo_client = AsyncMongoMockClient()
 mongo_db = mongo_client.test
-# MINIO
-minio_client = MockMinioClient(
-    ProjectConfig.MINIO_URL,
-    access_key=ProjectConfig.MINIO_ROOT_USER,
-    secret_key=ProjectConfig.MINIO_ROOT_PASSWORD,
-    secure=False
-)
-minio_client.make_bucket('data')
+
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -43,6 +38,18 @@ async def init_and_clear_mongo():
     list_collections = await mongo_db.list_collection_names()
     for collection in list_collections:
         await mongo_db.drop_collection(collection)
+
+
+@pytest.fixture(scope='function', autouse=True)
+async def init_and_clear_minio(minio_mock, mocker):
+    client_minio = Minio(
+        ProjectConfig.MINIO_URL,
+        access_key=ProjectConfig.MINIO_ROOT_USER,
+        secret_key=ProjectConfig.MINIO_ROOT_PASSWORD,
+        secure=False
+    )
+    mocker.patch('src.file_sharing.services.minio_service.client_minio', client_minio)
+    yield
 
 
 @pytest.fixture
